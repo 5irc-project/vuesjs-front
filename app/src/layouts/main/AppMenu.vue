964ad90655ad3b4eb08cb7578bb11862b51ec792
Zennyth <mathis.figuet@orange.fr>
2023-01-10 08:54:32 +0100

    add menu

--- app/src/layouts/main/AppMenu.vue
+++ app/src/layouts/main/AppMenu.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="layout-menu">
+    <router-link :to="{ name: 'home' }" class="layout-menu_link">
+      <i class="pi pi-home"></i>
+      <span>{{ $t('shared.home') }}</span>
+    </router-link>
+
+    <router-link :to="{ name: 'home' }" class="layout-menu_link">
+      <i class="pi pi-search"></i>
+      <span>{{ $t('shared.search') }}</span>
+    </router-link>
+
+    <GooeyButton>
+      <router-link :to="{ name: 'home' }" class="layout-menu_link">
+        <i class="pi pi-bolt" style="font-size: 1.75rem;"></i>
+        <span></span>
+      </router-link>
+    </GooeyButton>
+
+    <router-link :to="{ name: 'home' }" class="layout-menu_link">
+      <i class="pi pi-bookmark"></i>
+      <span>{{ $t('shared.playlists') }}</span>
+    </router-link>
+
+    <router-link :to="{ name: 'home' }" class="layout-menu_link">
+      <i class="pi pi-user"></i>
+      <span>{{ $t('shared.profil') }}</span>
+    </router-link>
   </div>
 </template>
 
 <script setup>
+import GooeyButton from '@/components/shared/buttons/GooeyButton.vue';
 </script>
--- app/src/router/routes/main.js
+++ app/src/router/routes/main.js
@@ -3,5 +3,12 @@ export default [
     path: "/",
     name: "main",
     component: () => import("@/layouts/main/AppLayout.vue"),
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        component: () => import("@/pages/Home.vue")
+      },
+    ]
   },
 ]
