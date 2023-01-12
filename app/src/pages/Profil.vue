a40517736b583473981e82ef08fe130b5a1492ee
Zennyth <mathis.figuet@@orange.com>
2023-01-12 14:49:30 +0100

    fix Profil view

--- app/src/pages/Profil.vue
+++ app/src/pages/Profil.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>{{ $t('shared.profil') }}</div>
-  <Button @click="userStore.logout" :label="$t('shared.disconnect')" icon="pi pi-sign-out" class="p-button-secondary" />
+  <div>
+    <div>{{ $t('shared.profil') }}</div>
+    <Button @click="userStore.logout" :label="$t('shared.disconnect')" icon="pi pi-sign-out" class="p-button-secondary" />
+  </div>
 </template>
 
 <script setup>
