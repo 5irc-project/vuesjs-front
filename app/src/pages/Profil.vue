c7f803758c7b2568746acecf48021fcd61f6efe1
Zennyth <mathis.figuet@@orange.com>
2023-01-12 14:58:57 +0100

    add user name to profil

--- app/src/pages/Profil.vue
+++ app/src/pages/Profil.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="page">
-    <div>{{ $t('shared.profil') }}</div>
+    <h3>{{ $t('shared.profil') }}</h3>
+    <h1> {{ userStore.getUser.display_name }}</h1>
     <Button @click="userStore.logout" :label="$t('shared.disconnect')" icon="pi pi-sign-out" class="p-button-secondary" />
   </div>
 </template>
