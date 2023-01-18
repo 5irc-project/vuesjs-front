c033b4f0b65d84d666b8ef355e3c6982c097d693
Giing <lazareth.baptiste@gmail.com>
2023-01-18 17:02:55 +0100

    add socket

--- app/src/ws/index.js
+++ app/src/ws/index.js
@@ -1,69 +1,40 @@
 import socketIO from 'socket.io-client';
+import { watch } from 'vue';
+import { storeToRefs } from "pinia";
+import { useUserStore } from '@/store/modules/user';
+
+
+
+export default {
+  install() {
+    console.log("hello")
+    const userStore = useUserStore();
+    const { user } = storeToRefs(userStore);
+
+    
+    watch(user, (newUser) => {
+      console.log("hzloo", newUser)
+    })
+  }
+}
+
 
 // export const socket = socketIO.connect('http://localhost/', {path: "/nodejs/socket.io"});
-export const socket = socketIO.connect('http://localhost:8080', {transports: ["websocket"]});
+export const socket = socketIO.connect('http://localhost:8081', {transports: ["websocket"]});
+
+export function onUserLogin(userId) {
+  console.log("userId : ", userId)
+  socket.on(userId, () => {
+    console.log("oui")
+  })
+
+}
 
 socket.on("notification", (body) => {
   console.log(body);
 })
 
-socket.on("message", (message) => {
-  store.dispatch(addMessage(message));
-})
-
-socket.on("newConnectedUser", (userInfo) => {
-  store.dispatch(addNewUser(userInfo));
-})
-
-socket.on("userDisconnected", (userInfo) => {
-  store.dispatch(removeUser(userInfo));
-})
 
 socket.on("error", (err) => {
   console.error("timeout: ", err);
 });
-
-window.socket = socket;
-
-const WS = {
-  // async login(user) {
-  //   socket.emit("login", user);
-
-  //   const { data } = await getConnectedUsers();
-    
-  //   const idUser = selectUserId(store.getState())
-  //   store.dispatch(initConnectedUsers(data.filter(u => u.id !== idUser)));
-
-  //   if (idUser != null) {
-  //     socket.on(idUser, async (object) => {
-
-  //       const notification = JSON.parse(object)
-  //       console.log("NOTIFICATION RECEIVED", JSON.parse(object));
-
-  //       if (notification.notificationAction === "UPDATE") {
-  //         try {
-  //           await getUserPendingTransaction(idUser)
-  //           await updateUserMoney(idUser)
-  //         } catch (error) {
-  //           console.error(error)
-  //         }
-  //       } else {
-  //         //TODO: notification toast
-  //         alert(object.message)
-  //       }
-  //     })
-  //   }
-
-  // },
-  // sendMessage(text) {
-  //   const destinationId = selectChatPal(store.getState())?.socketId;
-
-  //   socket.emit("message", {
-  //     destination: destinationId,
-  //     timestamp: new Date(),
-  //     text
-  //   });
-  // },
-}
-
-export default WS
